be47b970dae777aaea389de32f35512e0c5f3e4e
nur-nayeem <nurnayem768@gmail.com>
2025-10-02 15:14:57 +0600

    initialize react project

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import './App.css'
+import Navbar from './components/nav-bar/Navbar'
+import Footer from './components/footer/Footer'
+import { Outlet } from 'react-router'
+const App = () => {
+  return (
+    <>
+      <Navbar />
+      <main className='container mx-auto min-h-[calc(100vh-284px)]'>
+        <Outlet />
+      </main>
+      <Footer />
+    </>
+  )
+}
+
+export default App
