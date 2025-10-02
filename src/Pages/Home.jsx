dceca3cf6b77b8164343e93cdf1d0bdb33335af5
nur-nayeem <nurnayem768@gmail.com>
2025-10-02 15:53:54 +0600

    fix loader data

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -4,8 +4,7 @@ import Books from '../components/Books/Books'
 import { useLoaderData } from 'react-router'
 
 const Home = () => {
-
-
+    const data = useLoaderData()
     return (
         <>
             <Banner />
